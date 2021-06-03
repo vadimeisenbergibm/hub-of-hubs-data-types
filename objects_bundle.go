e32476692fda94bd3e15efe675d6715203a0ea11
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-03 15:23:00 +0300

    added objects bundle type

---
+++ objects_bundle.go
@@ -0,0 +1,22 @@
+package hub_of_hubs_data_types
+
+type ObjectsBundle struct {
+	Objects 		[]interface{} `json:"objects"`
+	DeletedObjects 	[]interface{} `json:"deletedObjects"`
+}
+
+func NewObjectBundle() *ObjectsBundle {
+	bundle := ObjectsBundle {
+		Objects: make([]interface{}, 0),
+		DeletedObjects: make([]interface{}, 0),
+	}
+	return &bundle
+}
+
+func (bundle *ObjectsBundle) AddObject(object interface{}) {
+	bundle.Objects = append(bundle.Objects, object)
+}
+
+func (bundle *ObjectsBundle) AddDeletedObject(object interface{}) {
+	bundle.DeletedObjects = append(bundle.DeletedObjects, object)
+}
