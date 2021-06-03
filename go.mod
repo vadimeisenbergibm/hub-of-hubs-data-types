4fda9c62696417df201bf8c5821377c19d2eedb0
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-03 11:44:11 +0300

    initial version - hoh data types
    to be used by the transport layer. both sides (hoh and lh) transport layer should
    use data types from this package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/open-cluster-management/hub-of-hubs-data-types
+
+go 1.16
---
+++ msg_types.go
@@ -0,0 +1,7 @@
+package hub_of_hubs_data_types
+
+const (
+	PolicyType = "PoliciesBundle"
+	PlacementRuleType = "PlacementRulesBundle"
+	PlacementBindingType = "PlacementBindingsBundle"
+)
