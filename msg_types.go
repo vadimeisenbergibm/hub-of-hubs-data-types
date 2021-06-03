e78b804f08524ebd65f8d7bd60e9ac85fd1a3ac0
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-03 14:22:39 +0300

    added GetMsgTypes func

--- msg_types.go
+++ msg_types.go
@@ -5,3 +5,11 @@ const (
 	PlacementRuleType = "PlacementRulesBundle"
 	PlacementBindingType = "PlacementBindingsBundle"
 )
+
+func GetBundleTypes() []string {
+	var bundleTypes []string
+	bundleTypes = append(bundleTypes, PolicyType)
+	bundleTypes = append(bundleTypes, PlacementRuleType)
+	bundleTypes = append(bundleTypes, PlacementBindingType)
+
+}
