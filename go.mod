4b8012f9d88b0385120c16977547013e15acb031
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-06 15:54:13 +0300

    revert of last commit

--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module github.com/open-cluster-management/hub-of-hubs-data-types
 
 go 1.16
-
-require (
-    sigs.k8s.io/controller-runtime v0.8.3
-)
--- objects_bundle.go
+++ objects_bundle.go
@@ -1,23 +1,21 @@
 package datatypes
 
-import "sigs.k8s.io/controller-runtime/pkg/client"
-
 type ObjectsBundle struct {
-	Objects        []client.Object `json:"objects"`
-	DeletedObjects []client.Object `json:"deletedObjects"`
+	Objects        []interface{} `json:"objects"`
+	DeletedObjects []interface{} `json:"deletedObjects"`
 }
 
 func NewObjectBundle() *ObjectsBundle {
 	return &ObjectsBundle {
-		Objects:        make([]client.Object, 0),
-		DeletedObjects: make([]client.Object, 0),
+		Objects:        make([]interface{}, 0),
+		DeletedObjects: make([]interface{}, 0),
 	}
 }
 
-func (bundle *ObjectsBundle) AddObject(object client.Object) {
+func (bundle *ObjectsBundle) AddObject(object interface{}) {
 	bundle.Objects = append(bundle.Objects, object)
 }
 
-func (bundle *ObjectsBundle) AddDeletedObject(object client.Object) {
+func (bundle *ObjectsBundle) AddDeletedObject(object interface{}) {
 	bundle.DeletedObjects = append(bundle.DeletedObjects, object)
 }
