81395599fef9372f0da14f0e61cb34c507359ff2
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-06 15:09:35 +0300

    changed to k8s client Object interface

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/open-cluster-management/hub-of-hubs-data-types
 
 go 1.16
+
+require (
+    sigs.k8s.io/controller-runtime v0.8.3
+)
--- objects_bundle.go
+++ objects_bundle.go
@@ -1,21 +1,23 @@
 package datatypes
 
+import "sigs.k8s.io/controller-runtime/pkg/client"
+
 type ObjectsBundle struct {
-	Objects 		[]interface{} `json:"objects"`
-	DeletedObjects 	[]interface{} `json:"deletedObjects"`
+	Objects        []client.Object `json:"objects"`
+	DeletedObjects []client.Object `json:"deletedObjects"`
 }
 
 func NewObjectBundle() *ObjectsBundle {
 	return &ObjectsBundle {
-		Objects: make([]interface{}, 0),
-		DeletedObjects: make([]interface{}, 0),
+		Objects:        make([]client.Object, 0),
+		DeletedObjects: make([]client.Object, 0),
 	}
 }
 
-func (bundle *ObjectsBundle) AddObject(object interface{}) {
+func (bundle *ObjectsBundle) AddObject(object client.Object) {
 	bundle.Objects = append(bundle.Objects, object)
 }
 
-func (bundle *ObjectsBundle) AddDeletedObject(object interface{}) {
+func (bundle *ObjectsBundle) AddDeletedObject(object client.Object) {
 	bundle.DeletedObjects = append(bundle.DeletedObjects, object)
 }
