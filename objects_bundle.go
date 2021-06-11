120219ef05694428d6f95cf5cfc39197856be208
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-11 11:17:40 +0300

    added function to bundle interface

--- objects_bundle.go
+++ objects_bundle.go
@@ -14,6 +14,7 @@ type Object interface {
 type Bundle interface {
 	AddObject(object Object)
 	AddDeletedObject(object Object)
+	GetObjectsBundle() *ObjectsBundle
 }
 
 func NewObjectBundle() *ObjectsBundle {
