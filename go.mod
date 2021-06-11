3def3bdadafdcc4cee7d8804265aa3145e9941e1
Nir Rozenbaum <nirro@il.ibm.com>
2021-06-11 20:47:26 +0300

    reverted bundle interface, changed back to generic interface

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/open-cluster-management/hub-of-hubs-data-types
 
 go 1.16
-
-require k8s.io/apimachinery v0.20.5
--- objects_bundle.go
+++ objects_bundle.go
@@ -1,30 +1,13 @@
 package datatypes
 
-import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
-)
-
-// represents k8s object
-type Object interface {
-	metav1.Object
-	runtime.Object
-}
-
-type Bundle interface {
-	AddObject(object Object)
-	AddDeletedObject(object Object)
-	GetObjectsBundle() *ObjectsBundle
-}
-
 func NewObjectBundle() *ObjectsBundle {
 	return &ObjectsBundle {
-		Objects:        make([]Object, 0),
-		DeletedObjects: make([]Object, 0),
+		Objects:        make([]interface{}, 0),
+		DeletedObjects: make([]interface{}, 0),
 	}
 }
 
 type ObjectsBundle struct {
-	Objects        []Object `json:"objects"`
-	DeletedObjects []Object `json:"deletedObjects"`
+	Objects        []interface{} `json:"objects"`
+	DeletedObjects []interface{} `json:"deletedObjects"`
 }
