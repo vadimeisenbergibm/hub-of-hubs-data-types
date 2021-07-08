7349cdabf04e295e6c44c59e546021eaeb2a80c6
Nir Rozenbaum <nirro@il.ibm.com>
2021-07-08 16:36:45 +0300

    moved remediation action from compliance status bundle to clusters per policy bundle

--- bundle/status/base_clusters_per_policy_bundle.go
+++ bundle/status/base_clusters_per_policy_bundle.go
@@ -1,9 +1,12 @@
 package status
 
+import v1 "github.com/open-cluster-management/governance-policy-propagator/pkg/apis/policy/v1"
+
 type ClustersPerPolicy struct {
-	PolicyId        string   `json:"policyId"`
-	Clusters        []string `json:"clusters"`
-	ResourceVersion string   `json:"resourceVersion"`
+	PolicyId          string               `json:"policyId"`
+	Clusters          []string             `json:"clusters"`
+	RemediationAction v1.RemediationAction `json:"remediationAction"`
+	ResourceVersion   string               `json:"resourceVersion"`
 }
 
 type BaseClustersPerPolicyBundle struct {
--- bundle/status/base_compliance_status_bundle.go
+++ bundle/status/base_compliance_status_bundle.go
@@ -1,15 +1,10 @@
 package status
 
-import (
-	"github.com/open-cluster-management/governance-policy-propagator/pkg/apis/policy/v1"
-)
-
 type PolicyComplianceStatus struct {
-	PolicyId                  string               `json:"policyId"`
-	NonCompliantClusters      []string             `json:"nonCompliantClusters"`
-	UnknownComplianceClusters []string             `json:"unknownComplianceClusters"`
-	RemediationAction         v1.RemediationAction `json:"remediationAction"`
-	ResourceVersion           string               `json:"resourceVersion"`
+	PolicyId                  string   `json:"policyId"`
+	NonCompliantClusters      []string `json:"nonCompliantClusters"`
+	UnknownComplianceClusters []string `json:"unknownComplianceClusters"`
+	ResourceVersion           string   `json:"resourceVersion"`
 }
 
 type BaseComplianceStatusBundle struct {
