53f983765d9877b99ecf80fec283ca71ed542a02
Nir Rozenbaum <nirro@il.ibm.com>
2021-07-19 13:07:26 +0300

    added comments

--- msg_types.go
+++ msg_types.go
@@ -1,10 +1,12 @@
 package datatypes
 
 const (
+	// message types
 	SpecBundle   = "SpecBundle"
 	StatusBundle = "StatusBundle"
 	Config       = "Config"
 
+	// message IDs
 	ManagedClustersMsgKey   = "ManagedClusters"
 	ClustersPerPolicyMsgKey = "ClustersPerPolicy"
 	PolicyComplianceMsgKey  = "PolicyCompliance"
