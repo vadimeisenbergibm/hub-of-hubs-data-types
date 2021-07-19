b368c3d4f7d6fa233fb6bae38383200cbf789c4a
Nir Rozenbaum <nirro@il.ibm.com>
2021-07-19 13:06:31 +0300

    added config message type

--- msg_types.go
+++ msg_types.go
@@ -3,6 +3,7 @@ package datatypes
 const (
 	SpecBundle   = "SpecBundle"
 	StatusBundle = "StatusBundle"
+	Config       = "Config"
 
 	ManagedClustersMsgKey   = "ManagedClusters"
 	ClustersPerPolicyMsgKey = "ClustersPerPolicy"
